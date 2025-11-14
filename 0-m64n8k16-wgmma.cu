8ee6d874ecd5163af6be11e601003b9df45c6e56
Kai McClennen <kmcclenn@mit.edu>
2025-11-14 17:41:15 -0500

    part 0

--- 0-m64n8k16-wgmma.cu
+++ 0-m64n8k16-wgmma.cu
@@ -17,27 +17,99 @@ typedef __nv_bfloat16 bf16;
 ////////////////////////////////////////////////////////////////////////////////
 // Part 0: No Swizzle WGGMA load for M = 64, N = 8, K = 16
 ////////////////////////////////////////////////////////////////////////////////
-
+#define CORE 8
 template <int TILE_M, int TILE_N, int TILE_K>
-__global__ void wgmma_m64n8k16(bf16 *a, bf16 *b, float *c) {
+__global__ void wgmma_m64n8k16(bf16 *a, bf16 *b, float *c)
+{
 
-    // <--- your code here --->
+    extern __shared__ bf16 shmem[];
+    bf16 *a_vals = shmem;
+    bf16 *b_vals = a_vals + TILE_M * TILE_K;
 
-} 
+    int idx = threadIdx.y * blockDim.x + threadIdx.x;
 
-template <int TILE_M, int TILE_N, int TILE_K>
-void launch_wgmma_m64n8k16(bf16 *a, bf16 *b, float *c) {
-    
-    // <--- your code here --->
+    for (int i = idx; i < TILE_M * TILE_K; i += blockDim.x * blockDim.y)
+    {
+        int row = i / TILE_K;
+        int col = i % TILE_K;
 
+        int chunk_row = row / CORE;
+        int chunk_col = col / CORE;
+
+        int in_row = row % CORE;
+        int in_col = col % CORE;
+
+        int chunk_size = CORE * CORE;
+        int chunks_k = TILE_K / CORE;
+
+        int shmem_idx = (chunk_row * chunks_k + chunk_col) * chunk_size + in_row * CORE + in_col;
+
+        a_vals[shmem_idx] = a[i];
+    }
+
+    for (int i = idx; i < TILE_K * TILE_N; i += blockDim.x * blockDim.y)
+    {
+        int row = i / TILE_K;
+        int col = i % TILE_K;
+
+        int chunk_row = row / CORE;
+        int chunk_col = col / CORE;
+
+        int in_row = row % CORE;
+        int in_col = col % CORE;
+
+        int chunk_size = CORE * CORE;
+        int chunks_k = TILE_K / CORE;
+
+        int shmem_idx = (chunk_row * chunks_k + chunk_col) * chunk_size + in_row * CORE + in_col;
+
+        b_vals[shmem_idx] = b[i];
+    }
+
+    __syncthreads();
+
+    uint64_t a_desc = make_smem_desc<NO_SWIZZLE>(a_vals, 128, 256);
+    uint64_t b_desc = make_smem_desc<NO_SWIZZLE>(b_vals, 128, 256);
+
+    float d[4] = {0.0f, 0.0f, 0.0f, 0.0f};
+
+    warpgroup_arrive();
+
+    wgmma_n8<1, 1, 1, 0, 0>(a_desc, b_desc, d);
+    wgmma_commit();
+    wgmma_wait<0>();
+
+    int x = threadIdx.x;
+    int y = threadIdx.y;
+
+    for (int i = 0; i < 2; i++)
+    {
+        int m = 16 * y + (8 * i) + x / 4;
+        for (int j = 0; j < 2; j++)
+        {
+            int n = (x % 4) * 2 + j;
+            c[n * TILE_M + m] = d[i * 2 + j];
+        }
+    }
 }
 
+template <int TILE_M, int TILE_N, int TILE_K>
+void launch_wgmma_m64n8k16(bf16 *a, bf16 *b, float *c)
+{
+
+    // <--- your code here --->
+    dim3 threads = dim3(32, 4); // one group
+    int shmem_size = (TILE_M * TILE_K + TILE_K * TILE_N) * sizeof(bf16);
+
+    wgmma_m64n8k16<TILE_M, TILE_N, TILE_K><<<1, threads, shmem_size>>>(a, b, c);
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 ///          YOU DO NOT NEED TO MODIFY THE CODE BELOW HERE.                  ///
 ////////////////////////////////////////////////////////////////////////////////
 
-int main() {
+int main()
+{
     const int M = 64;
     const int N = 8;
     const int K = 16;
@@ -45,13 +117,17 @@ int main() {
     // Initialize source matrix on host
     bf16 *a = (bf16 *)malloc(M * K * sizeof(bf16));
     bf16 *b = (bf16 *)malloc(N * K * sizeof(bf16));
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < K; j++) {
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < K; j++)
+        {
             a[i * K + j] = i + j;
         }
     }
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < K; j++) {
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < K; j++)
+        {
             b[j * N + i] = i + j;
         }
     }
@@ -66,10 +142,13 @@ int main() {
 
     // Compute CPU reference
     float *cpu_output = (float *)malloc(M * N * sizeof(float));
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < N; j++) {
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             float temp = 0.0f;
-            for (int k = 0; k < K; k++) {
+            for (int k = 0; k < K; k++)
+            {
                 float a_row = (float)a[i * K + k];
                 float a_col = (float)b[k + j * K];
                 temp += a_row * a_col;
@@ -79,7 +158,8 @@ int main() {
     }
 
     float *gpu_output = (float *)malloc(M * N * sizeof(float));
-    for (int i = 0; i < M * N; i++) {
+    for (int i = 0; i < M * N; i++)
+    {
         gpu_output[i] = 0;
     }
     cudaMemcpy(d_c, gpu_output, M * N * sizeof(float), cudaMemcpyHostToDevice);
@@ -93,8 +173,10 @@ int main() {
 
     // check results
     bool correct = true;
-    for (int idx = 0; idx < M * N; idx++) {
-        if (fabs(cpu_output[idx] - gpu_output[idx]) > 0.01f) {
+    for (int idx = 0; idx < M * N; idx++)
+    {
+        if (fabs(cpu_output[idx] - gpu_output[idx]) > 0.01f)
+        {
             correct = false;
             int j = idx / M;
             int i = idx % M;
